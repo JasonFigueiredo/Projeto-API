653ef0213fd801dd72ea1bce560f114c56ccb9df
Jason Lopes <jasonlopes132@gmail.com>
2025-02-25 20:07:49 -0300

    Add DB conection

---
+++ src/VO/FuncionarioVO.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Src\VO;
+
+use Src\_Public\Util;
+use Src\VO\UsuarioVO;
+
+class FuncionarioVO extends UsuarioVO
+{
+    private $id_setor;
+
+    public function setIdSetor($id_setor): void
+    {
+        $this->id_setor = Util::TratarDados($id_setor);
+    }
+    public function getIdSetor(): int
+    {
+        return $this->id_setor;
+    }
+}
--- src/VO/UsuarioVO.php
+++ src/VO/UsuarioVO.php
@@ -1,73 +1,87 @@
 <?php
 
-    namespace Src\VO;
+namespace Src\VO;
 
-    use Src\_Public\Util;
+use Src\_Public\Util;
 
-    class UsuarioVO{
+class UsuarioVO
+{
 
-        private $id;
-        private $nome;
-        private $tipo;
-        private $email;
-        private $tel;
-        private $senha;
-        private $status;
+    private $id;
+    private $nome;
+    private $tipo;
+    private $email;
+    private $tel;
+    private $senha;
+    private $status;
 
-        // get e set geral dos itens em private UsuarioVO
-        public function setId($id){
-            $this->id = $id;
-        }
-
-        public function getId() : int{
-            return $this->id;
-        }
-
-        public function setNome($nome){
-            $this->nome = Util::TratarDados($nome);
-        }
-
-        public function getNome() : string{
-            return $this->nome;
-        }
-
-        public function setTipo($tipo){
-            $this->tipo = Util::TratarDados($tipo);
-        }  
-
-        public function getTipo() : int{
-            return $this->tipo;
-        }
-
-        public function setEmail($email) : void{
-            $this->email = Util::RemoverTags($email);
-        }
-
-        public function getEmail() : string{
-            return $this->email;
-        }
-
-        public function setTel($tel){
-            $this->tel = Util::TratarDados($tel);
-        }
-
-        public function getTel() : string{
-            return $this->tel;
-        }
-
-        public function setSenha($senha) : void{
-            $this->senha = Util::RemoverTags($senha);
-        }
-
-        public function getSenha() : string{
-            return $this->senha;
-        }
-
-        public function setStatus($status) : void{
-            $this->status = $status;
-        }
-
-        public function getStatus() : int{
-            return $this->status;
-        }
+    // get set de ID
+    public function setId($id)
+    {
+        $this->id = $id;
     }
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    // get set de NOME
+    public function setNome($nome)
+    {
+        $this->nome = Util::TratarDados($nome);
+    }
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    // get set de TIPO
+    public function setTipo($tipo)
+    {
+        $this->tipo = Util::TratarDados($tipo);
+    }
+    public function getTipo(): int
+    {
+        return $this->tipo;
+    }
+
+    // get set de EMAIL
+    public function setEmail($email): void
+    {
+        $this->email = Util::RemoverTags($email);
+    }
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    // get set de TELEFONE
+    public function setTel($tel): void
+    {
+        $this->tel = Util::TirarCaracteresEspeciais($tel);
+    }
+    public function getTel(): string
+    {
+        return $this->tel;
+    }
+
+    // get set de SENHA
+    public function setSenha($senha): void
+    {
+        $this->senha = Util::RemoverTags($senha);
+    }
+    public function getSenha(): string
+    {
+        return $this->senha;
+    }
+
+    // get e set de STATUS
+    public function setStatus($status): void
+    {
+        $this->status = $status;
+    }
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+}
