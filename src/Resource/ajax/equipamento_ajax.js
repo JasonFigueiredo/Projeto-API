037ba617c9c54c3468ad744ac1d68e80021b7be4
JasonFigueiredo <jasonlopes132@gmail.com>
2025-05-19 15:45:57 -0300

    Update equipamento_ajax.js
    adicionado a function carregar modelo de equipamento
    '

--- src/Resource/ajax/equipamento_ajax.js
+++ src/Resource/ajax/equipamento_ajax.js
@@ -1,18 +1,35 @@
-function CarregarTipo(){
-  $.ajax({
-    beforeSend: function (){
-      loand():
-    },
-    type:'post',
-    url: BASE_URL_DATAVIEW("equipamento_dataview"),
-    data:{
-      carregar_tipos:"ajx"
-    },
-    success: function(dados){
-      $("$tipo").hrml(dados);
-    },
-    complete: function(){
-      RemoverLoand();
-    }
-    )}
+function CarregarTipo() {
+    $.ajax({
+        beforeSend: function () {
+            loand();
+        },
+        type: 'post', url: BASE_URL_DATAVIEW("equipamento_dataview"),
+        data: {
+            carregar_tipos: "ajx"
+        },
+        success: function (dados) {
+            $("$tipo").hrml(dados);
+        },
+        complete: function () {
+            RemoverLoand();
+        }
+    })
+}
+
+function CarregarModelos() {
+    $.ajax({
+        beforeSend: function () {
+            loand();
+        },
+        type: 'post', url: BASE_URL_DATAVIEW("equipamento_dataview"),
+        data: {
+            carregara_modelos: "ajx"
+        },
+        success: function (dados) {
+            $("$modelo").hrml(dados);
+        },
+        complete: function () {
+            RemoverLoand();
+        }
+    })
 }
