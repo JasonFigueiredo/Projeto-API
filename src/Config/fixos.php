66db08a0acdebf173a216236e682a2f8e2d52f27
Jason Lopes <jasonlopes132@gmail.com>
2025-07-22 20:36:55 -0300

    Organiza e adiciona comentários nas constantes de modelo e situação de equipamento

--- src/Config/fixos.php
+++ src/Config/fixos.php
@@ -7,20 +7,24 @@
     const ALTERAR_TIPO_EQUIPAMENTO = "Alterar Tipo de Equipamento";
     const EXCLUIR_TIPO_EQUIPAMENTO = "Excluir Tipo de Equipamento";
 
+    // funções de modelo de equipamento
     const CADASTRAR_MODELO_EQUIPAMENTO = "Cadastrar Modelo de Equipamento";
     const ALTERAR_MODELO_EQUIPAMENTO = "Alterar Modelo de Equipamento";
     const EXCLUIR_MODELO_EQUIPAMENTO = "Excluir Modelo de Equipamento";
 
+    // funções de equipamento
     const CADASTRAR_EQUIPAMENTO = "Cadastrar Equipamento";
     const ALTERAR_EQUIPAMENTO = "Alterar Equipamento";
     const EXCLUIR_EQUIPAMENTO = "Excluir Novo Equipamento";
     const DESCARTAR_EQUIPAMENTO = "Descartar Equipamento";
     const ALOCAR_EQUIPAMENTO = "Alocar Equipamento";
 
+    // Flags de situação de equipamento
     const SITUACAO_EQUIPAMENTO_ALOCADO = 1;
     const SITUACAO_EQUIPAMENTO_REMOVIDO = 2;
     const SITUACAO_EQUIPAMENTO_MANUTENCAO = 3;
 
+    // Situações de equipamento
     const SITUACAO_ATIVO = 1;
     const SITUACAO_INATIVO = 0;
     const SITUACAO_DESCARTADO = 0;
